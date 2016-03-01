5d0232596df67c160a8d18fb86259da567817045
Kayleigh <Kayleigh@192.168.1.191>
2016-03-01 14:23:33 -0800

    Update

--- src/ArrayStats.java
+++ src/ArrayStats.java
@@ -2,26 +2,53 @@
 import java.util.*;
 
 public class ArrayStats {
+	
 	public static void main(String args[]){
 		
 	Random number = new Random();
 	
 	//initializing array with 300 spaces
-	double[] myArray = new double[300];
+	int[][] myArray = new int[15][20];
+	int row, col;
 	
-	int filling = 0;
-	
-	//loop which fills array with random numbers
-	while(filling < 300){
-		int randomNumber = number.nextInt(301);
-		myArray[filling] = randomNumber;
-		filling++;
+	for(row = 0; row < myArray.length; row++){
+		for(col = 0; col < myArray[row].length; col++){
+			int randi = number.nextInt(101);
+			myArray[row][col] = randi;
+			
+			System.out.print(myArray[row][col] + " ");
+			System.out.println();
+		}
 	}
 	
-	for (int i = 0; i<300; i++){
-		System.out.println(myArray[i]);
+	//Sorts Array
+	Arrays.sort(myArray);
+	
+	for (row = 0; row < 300; row++) {		
+		//THIS IS WHER YOU NEED TO MAKE A MATRIX!
+		System.out.println(myArray[row]);
+		
 	}
 	
-	System.out.println("THIS IS A TEST!");
-}
-}
+	
+	int fr[] = new int[myArray.length];
+	for (int i = 0; i < fr.length; i++){
+		fr[i] = 0;
+	}
+	for(int i = 0; i < myArray.length; i++){
+		for(int j = 0; j < myArray.length; j++){
+			if(myArray[i] == myArray[j]){
+				fr[i]++;
+			}
+		}
+	}
+	
+			}
+	
+	
+	}
+
+	
+
+	
+	
